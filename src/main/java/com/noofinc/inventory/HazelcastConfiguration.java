231e7dcddf339a591aadd75af99e5b965748063d
“a541676” <jim.beyers@target.com>
2014-11-23 22:00:42 -0600

    changing to tcp

--- src/main/java/com/noofinc/inventory/HazelcastConfiguration.java
+++ src/main/java/com/noofinc/inventory/HazelcastConfiguration.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 import com.hazelcast.config.ManagementCenterConfig;
+import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.spring.cache.HazelcastCacheManager;
@@ -20,11 +22,22 @@ public class HazelcastConfiguration {
 	@Value("${hz_url}")
 	private String hazelCastManagementUrl;
 
+	@Value("${hz_members}")
+	private String hazelcastMembers;
 
 	@Bean
 	public HazelcastInstance hazelcastInstance() {
 		Config config = new Config();
 		
+		
+		NetworkConfig networkConfig = config.getNetworkConfig();
+		networkConfig.setPort(5900);
+		networkConfig.setPortAutoIncrement(false);
+		JoinConfig join = networkConfig.getJoin();
+		join.getTcpIpConfig().addMember(hazelcastMembers);
+
+		join.getMulticastConfig().setEnabled(false);
+
 		ManagementCenterConfig managementCenterConfig = new ManagementCenterConfig();
 		managementCenterConfig.setEnabled(true);
 		managementCenterConfig.setUrl(hazelCastManagementUrl);
@@ -32,6 +45,8 @@ public class HazelcastConfiguration {
 		HazelcastInstance h = Hazelcast.newHazelcastInstance(config);
 		return h;
 	}
+	
+
 	
 	@Bean 
 	public HazelcastCacheManager cacheManager(HazelcastInstance hazelcastInstance){
