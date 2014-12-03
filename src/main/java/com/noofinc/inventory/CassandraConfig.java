663968b2426165e473dff7de5914ae80de217de9
“a541676” <jim.beyers@target.com>
2014-12-03 16:02:01 -0600

    brought the cassandra config into app config

--- src/main/java/com/noofinc/inventory/CassandraConfig.java
+++ src/main/java/com/noofinc/inventory/CassandraConfig.java
@@ -3,6 +3,7 @@ package com.noofinc.inventory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cassandra.core.CqlOperations;
 import org.springframework.cassandra.core.CqlTemplate;
 import org.springframework.context.annotation.Bean;
@@ -20,20 +21,32 @@ import org.springframework.data.cassandra.mapping.CassandraMappingContext;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
-@PropertySource(value = { "classpath:cassandra.properties" })
+//@PropertySource(value = { "classpath:cassandra.properties" })
 @EnableCassandraRepositories("com.noofinc.inventory.repositories")
 public class CassandraConfig extends AbstractCassandraConfiguration {
 	
 	
 	private static final Logger LOG = LoggerFactory.getLogger(CassandraConfig.class);
 
-	@Autowired
-	private Environment env;
+//	@Autowired
+//	private Environment env;
+	
+	@Value("${cassandra_keyspace}")
+	private String keyspaceName;
+	
+	
+	@Value("${cassandra_port}")
+	private int port;
+	
+	
+	@Value("${cassandra_contactpoints}")
+	private String contactPoints;
+	
 
 	@Override
 	protected String getKeyspaceName() {
 		// TODO Auto-generated method stub
-		return env.getProperty("cassandra.keyspace");
+		return keyspaceName;
 	}
 
 	@Bean
@@ -46,8 +59,10 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
 	public CassandraClusterFactoryBean cluster() {
 
 		CassandraClusterFactoryBean cluster = new CassandraClusterFactoryBean();
-		cluster.setContactPoints(env.getProperty("cassandra.contactpoints"));
-		cluster.setPort(Integer.parseInt(env.getProperty("cassandra.port")));
+		cluster.setContactPoints(contactPoints);
+		cluster.setPort(port);
+//		cluster.setContactPoints(env.getProperty("cassandra.contactpoints"));
+//		cluster.setPort(Integer.parseInt(env.getProperty("cassandra.port")));
 
 		LOG.info("Created CassandraClusterFactoryBean");
 		return cluster;
@@ -75,7 +90,8 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
 		
 		CassandraSessionFactoryBean session = new CassandraSessionFactoryBean();
 		session.setCluster(cluster().getObject());
-		session.setKeyspaceName(env.getProperty("cassandra.keyspace"));
+		session.setKeyspaceName(keyspaceName);
+//		session.setKeyspaceName(env.getProperty("cassandra.keyspace"));
 		session.setConverter(converter());
 		session.setSchemaAction(SchemaAction.CREATE);
 
